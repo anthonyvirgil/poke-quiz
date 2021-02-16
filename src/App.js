52ecac9f8a552c461bafaf4dca2794e1f0b76047
anthonyvirgil <anthonyvirgil.bermejo@gmail.com>
2021-02-16 00:26:30 -0500

    Small UI changes

--- src/App.js
+++ src/App.js
@@ -61,74 +61,68 @@ function App() {
 
 	return (
 		<>
-			<div className="App text-white bg-gray-900 font-custom p-5 min-h-screen overflow-y-auto">
-				<div className="flex-col flex-wrap justify-center items-center">
-					<div className="flex-1">
-						<h1 className="text-3xl m-4">POKé QUIZ</h1>
-						{!gameOver && userAnswers.length !== TOTAL_QUESTIONS && (
-							<p className="text-xl">Score: {score}</p>
+			<div className="App flex-col text-white bg-gray-900 font-custom p-5 min-h-screen overflow-y-auto justify-between">
+				<div className="flex-grow-1">
+					<h1 className="text-3xl m-4">POKé QUIZ</h1>
+					{!gameOver && userAnswers.length !== TOTAL_QUESTIONS && (
+						<p className="text-xl">Score: {score}</p>
+					)}
+					{!gameOver && userAnswers.length !== TOTAL_QUESTIONS && (
+						<p className="text-xl p-4">
+							Question: {questionNumber + 1} / {TOTAL_QUESTIONS}
+						</p>
+					)}
+					{userAnswers.length === TOTAL_QUESTIONS && (
+						<p className="text-xl">
+							Game Over! Your final score is {score}/{questionNumber + 1}!
+						</p>
+					)}
+					{(gameOver || userAnswers.length === TOTAL_QUESTIONS) && (
+						<>
+							<button
+								className="w-44 mt-4 mr-1 py-3 px-9 bg-gray-700 border border-black rounded-xl hover:bg-gray-800"
+								onClick={startQuiz}
+							>
+								{userAnswers.length === TOTAL_QUESTIONS
+									? 'Restart Quiz'
+									: 'Start Quiz'}
+							</button>
+						</>
+					)}
+					{loading && (
+						<p className="text-xl w-full mt-20">Loading Questions ... </p>
+					)}
+					{!loading && !gameOver && userAnswers.length !== TOTAL_QUESTIONS && (
+						<QuestionCard
+							answers={answerArray}
+							answerImage={answerPokemonImage}
+							actualAnswer={answerPokemonName}
+							questionNum={questionNumber + 1}
+							callback={checkAnswer}
+							userAnswers={userAnswers}
+							userAnswered={userAnswered}
+						/>
+					)}
+					{!gameOver &&
+						!loading &&
+						userAnswers.length === questionNumber + 1 &&
+						questionNumber !== TOTAL_QUESTIONS - 1 && (
+							<button
+								className="w-44 mt-2 py-3 px-9 bg-gray-700 border border-black rounded-xl hover:bg-gray-800"
+								onClick={nextQuestion}
+							>
+								Next Question
+							</button>
 						)}
-						{!gameOver && userAnswers.length !== TOTAL_QUESTIONS && (
-							<p className="text-xl p-4">
-								Question: {questionNumber + 1} / {TOTAL_QUESTIONS}
-							</p>
-						)}
-						{userAnswers.length === TOTAL_QUESTIONS && (
-							<p className="text-xl">
-								Game Over! Your final score is {score}/{questionNumber + 1}!
-							</p>
-						)}
-						{(gameOver || userAnswers.length === TOTAL_QUESTIONS) && (
-							<>
-								<button
-									className="w-44 mt-4 mr-1 py-3 px-9 bg-gray-700 border border-black rounded-xl hover:bg-gray-800"
-									onClick={startQuiz}
-								>
-									{userAnswers.length === TOTAL_QUESTIONS
-										? 'Restart Quiz'
-										: 'Start Quiz'}
-								</button>
-							</>
-						)}
-						{loading && (
-							<p className="text-xl w-full mt-20">Loading Questions ... </p>
-						)}
-						{!loading &&
-							!gameOver &&
-							userAnswers.length !== TOTAL_QUESTIONS && (
-								<QuestionCard
-									answers={answerArray}
-									answerImage={answerPokemonImage}
-									actualAnswer={answerPokemonName}
-									questionNum={questionNumber + 1}
-									callback={checkAnswer}
-									userAnswers={userAnswers}
-									userAnswered={userAnswered}
-								/>
-							)}
-						{!gameOver &&
-							!loading &&
-							userAnswers.length === questionNumber + 1 &&
-							questionNumber !== TOTAL_QUESTIONS - 1 && (
-								<button
-									className="w-44 mt-2 py-3 px-9 bg-gray-700 border border-black rounded-xl hover:bg-gray-800"
-									onClick={nextQuestion}
-								>
-									Next Question
-								</button>
-							)}
-					</div>
 				</div>
-				<div className="mt-20">
-					<p className="w-full m-auto text-center text-white">
+				<div className="mt-24">
+					<p className="text-center text-white">
 						API -{' '}
 						<a href="https://pokeapi.co/" className="underline">
 							PokéAPI
 						</a>
 					</p>
-					<p className="w-full m-auto text-center text-white">
-						Created by Anthony-Virgil Bermejo
-					</p>
+					<p className="text-center text-white">Created by @anthonyvirgil_</p>
 				</div>
 			</div>
 		</>
